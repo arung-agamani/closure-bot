b0c17de72077978aa4be85ddce61ae640cd6b4a7
Haruka Shirayuki <arung.agamani@gmail.com>
2020-05-01 02:34:36 +0700

    Created script for running bot, server, or both

--- index.js
+++ index.js
@@ -1,41 +1,33 @@
-// const Discord = require('discord.js');
 const dotenv = require('dotenv').config();
-// const fs = require('fs');
+const express = require('express');
 
 const Closure = require('./bot/closure');
 
-const botApp = new Closure();
-botApp.start(process.env.DISCORD_BOT_TOKEN);
-
-/* const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./bot/commands').filter(file => file.endsWith('.js'));
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-for (const file of commandFiles) {
-    const command = require(`./bot/commands/${file}`);
-    client.commands.set(command.name, command);
+if (process.env.BOT === '1') {
+    console.log("Starting bot.");
+    const botApp = new Closure();
+    botApp.start(process.env.DISCORD_BOT_TOKEN);
 }
 
-client.on('message', msg => {
-    if (!msg.content.startsWith('^') || msg.author.bot) return;
+if (process.env.SERVER === '1') {
+    console.log("Starting server.");
+    const server = express();
+    // server section
+    server.get('/', (req, res) => {
+        res.send(`Test`);
+    });
 
-    const args = msg.content.slice(1).split(/ +/);
-    const command = args.shift().toLowerCase();
-    console.log(command);
-    if (!client.commands.has(command)) return;
+    server.post('/closure', (req, res) => {
+        res.send('okay dokutah');
+    });
 
-    try {
-        client.commands.get(command).execute(msg, args);
-    } catch (error) {
-        console.error(`Error in executing "${command}" command!`);
-        msg.reply(`There is an error on that command, Doctor!\nCheck your server log.`);
-    }
-});
+    server.listen(2000, ()=> {
+        console.log('Server is on at port 2000');
+    });
+}
+
+
+// misc functions
 
 function setDefaultChannel(guildID, channelID) {
     const configFile = JSON.parse(fs.readFileSync('./bot/closure.json'));
@@ -43,7 +35,3 @@ function setDefaultChannel(guildID, channelID) {
     configFile.defaultChannelID = channelID;
     fs.writeFileSync('./bot/closure.json', JSON.stringify(configFile, null, 4));
 }
-
-// setDefaultChannel('aaaa', 'bbbb');
-
-client.login(process.env.DISCORD_BOT_TOKEN); */
